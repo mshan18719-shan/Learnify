87edd853b7e7eb3fd927c571e5cc46af5c3b37ed
mshan18719 <mshan18719@gmail.com>
2025-10-26 14:30:30 +0500

    Updated Learnify with latest changes

--- asserts/script/script.js
+++ asserts/script/script.js
@@ -1,45 +1,23 @@
-const Slider = document.getElementById('slider');
-const Previous = document.getElementById('previous');
-const Next = document.getElementById('next');
-const Card = document.getElementsByClassName('testimonial_slide')
+$(document).ready(function () {
+  var owl = $(".owl-carousel");
+  owl.owlCarousel({
+    loop: true,
+    margin: 20,
+    autoplay: false,
+    // autoplayTimeout: 3000,
+    // autoplayHoverPause: true,
+    dots: false,
+    responsive: {
+      0: { items: 1, nav: false },
+      768: { items: 1, nav: false },
+      992: { items: 1, nav: false },
+    },
+  });
 
-let slideNumber = 1;
-// console.log('hello Shan')
-// RIGHT SLIDER //
-const nextSlide = () => {
-   Slider.style.transform = `translateX(-${slideNumber * 100}%)`;
-        slideNumber ++;
-}
-
-const firstSlide = () => {
-    Slider.style.transform = `translateX(0%)`;
-        slideNumber = 1;
-}
-
-// LEFT SLIDER  //
-const prevSlide = () => {
-    Slider.style.transform = `translateX(-${(slideNumber - 2)*638}px)`
-        slideNumber --;
-}
-const lastSlide = () => {
-    Slider.style.transform = `translateX(-${(Card.length-1)* 638}px)`
-        slideNumber = Card.length
-}
-
-Next.addEventListener('click',()=>{
-    if(slideNumber < Card.length){
-        nextSlide();
-    }
-    else{
-        firstSlide();
-    }
+  $("#next").click(function () {
+    owl.trigger("next.owl.carousel");
+  });
+  $("#previous").click(function () {
+    owl.trigger("prev.owl.carousel");
+  });
 });
-
-Previous.addEventListener('click', ()=>{
-    if(slideNumber > 1){
-        prevSlide();
-    }
-    else{
-        lastSlide();
-    }
-})
